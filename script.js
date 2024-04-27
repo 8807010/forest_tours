7d8d8e440cbd671cb6dab3f5d7ad3478f4020251
Artiom_Sokolov <sokolova-net@mail.ru>
2024-04-27 23:19:05 +0300

    complite code

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slides = document.querySelectorAll('.slide');
+const prevIcon = document.querySelector('.bx-chevron-left');
+const numberofslides = slides.length;
+let slideNumber = 0;
+
 //slider next button
+nextBtn.onclick = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+    prevBtn.style.backgroundColor = "#fff";
+    prevIcon.style.color = "#444";
+  })
+  slideNumber++;
+  if (slideNumber > (numberofslides - 1)) {
+    slideNumber = 0;
+  }
+  slides[slideNumber].classList.add('active');
+}
 
 //slider prev button
+prevBtn.onclick = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+    prevBtn.style.backgroundColor = "#fff";
+    prevIcon.style.color = "#444";
+  })
+
+  slideNumber--;
+
+  if (slideNumber < 0) {
+    slideNumber = numberofslides - 1;
+  }
+
+  slides[slideNumber].classList.add('active');
+}
+console.log(prevIcon);
