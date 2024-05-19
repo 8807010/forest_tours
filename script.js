72abf5318bcf1071833cb0a80953657578a006ac
Artiom_Sokolov <sokolova-net@mail.ru>
2024-05-19 19:58:18 +0300

    add more information about forests

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
 const slides = document.querySelectorAll('.slide');
 const prevIcon = document.querySelector('.bx-chevron-left');
-const numberofslides = slides.length;
+const numberOfSlides = slides.length;
 let slideNumber = 0;
 
 //slider next button
@@ -14,7 +14,7 @@ nextBtn.onclick = () => {
     prevIcon.style.color = "#444";
   })
   slideNumber++;
-  if (slideNumber > (numberofslides - 1)) {
+  if (slideNumber > (numberOfSlides - 1)) {
     slideNumber = 0;
   }
   slides[slideNumber].classList.add('active');
@@ -31,9 +31,8 @@ prevBtn.onclick = () => {
   slideNumber--;
 
   if (slideNumber < 0) {
-    slideNumber = numberofslides - 1;
+    slideNumber = numberOfSlides - 1;
   }
 
   slides[slideNumber].classList.add('active');
 }
-console.log(prevIcon);
